59b0639f37b99b32acc8a43a44c5c4e08aa68927
Emilian <emilianevgenevishborisov@gmail.com>
2024-10-14 23:49:24 +0300

    add Teacher Registration

---
+++ all_buttons/inline_btn.py
@@ -0,0 +1,15 @@
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+def get_callback_btns(
+        *,
+        btns: dict[str, str],
+        sizes: tuple[int] = (1,)):
+    keyboard = InlineKeyboardBuilder()
+
+    for text, data in btns.items():
+        keyboard.add(InlineKeyboardButton(text=text, callback_data=data))
+
+    return keyboard.adjust(*sizes).as_markup()
+
