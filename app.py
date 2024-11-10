a8eafea64c22e550f30d72ce6599148d83137854
nastia-romashka <nastia_romashka@icloud.com>
2024-11-11 01:44:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,22 @@
 # Файл для создания бота
 import asyncio
+import os
+
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from config import bot_token
+from dotenv import find_dotenv, load_dotenv
+load_dotenv(find_dotenv())
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from handlers.teacher import teacher_router
+from handlers.student import student_router
 
 from all_buttons import buttons
 
-bot = Bot(token=bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+#список id админов
+bot.my_admins_list = []
+
 dp = Dispatcher()
 
 #функция, реагирующая на команду start
@@ -19,11 +26,9 @@ async def start_cmd(message: types.Message):
                          reply_markup=buttons.start_kb)
 
 dp.include_router(teacher_router)
+dp.include_router(student_router)
 
 async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
 asyncio.run(main())
-
-
-
