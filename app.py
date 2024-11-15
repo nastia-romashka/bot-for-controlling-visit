18bac11815255aa26936b97885060350c654902b
Emilian <emilianevgenevishborisov@gmail.com>
2024-11-15 16:42:56 +0300

    connect db

--- app.py
+++ app.py
@@ -5,30 +5,47 @@ import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from dotenv import find_dotenv, load_dotenv
+
 load_dotenv(find_dotenv())
 from aiogram.enums import ParseMode
 from aiogram.client.default import DefaultBotProperties
 from handlers.teacher import teacher_router
 from handlers.student import student_router
 
+from database import DataBase
+from config import host, password, user, db_name
+from hashlib import sha256
+import cryptography
+
 from all_buttons import buttons
 
 bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-#список id админов
+# список id админов
 bot.my_admins_list = []
 
 dp = Dispatcher()
 
-#функция, реагирующая на команду start
+# Создание базы данных
+db: DataBase = DataBase()
+db.create_tables()
+
+
+# функция, реагирующая на команду start
 @dp.message(CommandStart())
 async def start_cmd(message: types.Message):
     await message.answer("В роли кого вы будете использовать этого тг бота?",
                          reply_markup=buttons.start_kb)
 
+
 dp.include_router(teacher_router)
 dp.include_router(student_router)
+
 
 async def main():
     await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
 
+
 asyncio.run(main())
+
+# Закрытие базы данных
+db.close()
--- handlers/teacher.py
+++ handlers/teacher.py
@@ -9,17 +9,27 @@ from all_buttons.inline_btn import get_callback_btns
 from hashlib import sha256
 from config import default_password
 
+from database import DataBase
+
+from loguru import logger
+import sys
+
+logger.remove()
+logger.add(sys.stderr, level="DEBUG")
+
 pars: ParsingSUAIRasp = ParsingSUAIRasp()
 
 teacher_router = Router()
 teacher_router.message.filter(ChatTypeFilter(["private"]))
 
+db: DataBase = DataBase()
+
 
 class Teacher_Registration(StatesGroup):
     # Шаги состояний
-    name = State()
-    name_and_post = State()
-    password = State()
+    id: State = State()
+    name_and_post: State = State()
+    password: State = State()
 
     teacher_registration_step = None
 
@@ -33,23 +43,24 @@ class Teacher_Registration(StatesGroup):
 @teacher_router.message(F.text == "Преподаватель")
 async def starring(message: types.Message, state: FSMContext):
     await message.answer("Введите имя в формате: <i>Иванов И. И.</i>", reply_markup=types.ReplyKeyboardRemove())
-    await state.set_state(Teacher_Registration.name)
+    await state.set_state(Teacher_Registration.id)
 
 
-@teacher_router.message(Teacher_Registration.name, F.text)
+@teacher_router.message(Teacher_Registration.id, F.text)
 async def add_name(message: types.Message, state: FSMContext):
-    await state.update_data(name=message.text)
-    names: list= pars.get_names_and_post(message.text)
+    await state.update_data(id=message.chat.id)
+    names: list = pars.get_names_and_post(message.text)
 
-    name_post: dict[str,str] = {}
+    name_post: dict[str, str] = {}
 
     for i in range(len(names)):
         name_post[names[i]] = f'teacher_{message.text}_{i}'
 
-    await message.answer("Выберете себя",  reply_markup=get_callback_btns(btns=name_post))
+    await message.answer("Выберете себя", reply_markup=get_callback_btns(btns=name_post))
     await state.set_state(Teacher_Registration.name_and_post)
 
-@teacher_router.callback_query(Teacher_Registration.name_and_post,F.data.startswith("teacher_"))
+
+@teacher_router.callback_query(Teacher_Registration.name_and_post, F.data.startswith("teacher_"))
 async def name_post(callback: types.CallbackQuery, state: FSMContext):
     cd: list = callback.data.split("_")
     inf: list = pars.get_names_and_post(cd[-2])
@@ -60,16 +71,35 @@ async def name_post(callback: types.CallbackQuery, state: FSMContext):
     await callback.message.answer(f"Введите пароль")
     await state.set_state(Teacher_Registration.password)
 
+
 @teacher_router.message(Teacher_Registration.password, F.text)
 async def password(message: types.Message, state: FSMContext):
     hash_password = sha256(message.text.encode('utf-8')).hexdigest()
     if hash_password == default_password:
         await state.update_data(password=hash_password)
         Teacher_Registration.teacher_registration_step = None
+        data = await state.get_data()
         await state.clear()
         await message.answer("Вы вошли!")
+        # Надо доработать
+        FIO: str= data['name_and_post']
+        fio_arr = []
+        fio_arr.append(FIO[:FIO.find(' ')])
+        FIO = FIO[FIO.find(' ') + 1:]
+        fio_arr.append(FIO[:FIO.find('.')])
+        FIO = FIO[FIO.find('.') + 1:]
+        fio_arr.append(FIO[:FIO.find('.')])
+        FIO = FIO[FIO.find('.'):]
+        fio_arr.append(FIO[FIO.rfind('-') + 2:])
+
+        logger.debug(data['id'])
+        logger.debug(data['password'])
+        logger.debug(fio_arr)
+
+        db.add_teacher_in_db(array = fio_arr, tg_id=data['id'],password=data['password'])
     else:
         await message.answer("Пароль не верен. Попробуйте снова.")
+
 
 @teacher_router.message(StateFilter("*"), Command("отмена"))
 @teacher_router.message(StateFilter("*"), F.text.casefold() == "отмена")
@@ -81,3 +111,6 @@ async def cancel_handler(message: types.Message, state: FSMContext) -> None:
         Teacher_Registration.teacher_registration_step = None
     await state.clear()
     await message.answer("Действия отменены", reply_markup=start_kb)
+
+
+db.close()
