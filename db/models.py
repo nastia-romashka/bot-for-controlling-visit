a9d3136396cba8114131e6f08732937c196c31db
4215 Vakula A D <lordagenzzz@gmail.com>
2024-11-27 21:18:05 +0300

    added tables and queries

--- db/models.py
+++ db/models.py
@@ -12,7 +12,7 @@ class Teacher(Base):
     __tablename__  = 'teacher'
 
     # Поля таблицы и их параметры
-    teacherTelegram_id:Mapped[int] = mapped_column(primary_key=True)
+    teacherTelegram_id:Mapped[int] = mapped_column(Integer, primary_key=True)
     teacherSurname:Mapped[str] = mapped_column(String(30), use_existing_column=False)
     teacherName:Mapped[str] = mapped_column(String(30), use_existing_column=False)
     teacherPatronymic:Mapped[str] = mapped_column(String(30))
@@ -22,9 +22,37 @@ class Teacher(Base):
 class Lesson(Base):
     __tablename__ = 'lesson'
 
-    LessonId: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    lessonId: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     teacherTelegram_id: Mapped[int] = mapped_column(Integer, ForeignKey('teacher.teacherTelegram_id'))
-    LessonName: Mapped[str] = mapped_column(String(30), use_existing_column=False)
-    LessontType: Mapped[str] = mapped_column(String(3), use_existing_column=False)
-    Group: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    lessonName: Mapped[str] = mapped_column(String(30), use_existing_column=False)
+    lessonType: Mapped[str] = mapped_column(String(3), use_existing_column=False)
+    group: Mapped[int] = mapped_column(Integer, use_existing_column=False)
 
+class Student(Base):
+    __tablename__ = 'student'
+
+    studentId: Mapped[int] = mapped_column(Integer, primary_key=True)
+    studentGroup: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    studentSurname: Mapped[str] = mapped_column(String(30), use_existing_column=False)
+    studentName: Mapped[str] = mapped_column(String(30), use_existing_column=False)
+    studentPatronymic: Mapped[str] = mapped_column(String(30), use_existing_column=False)
+    studentTelegram_id: Mapped[int] = mapped_column(Integer)
+
+class Gradebook(Base):
+    __tablename__ = 'gradebook'
+
+    gradebookId: Mapped[int] = mapped_column(Integer, primary_key=True,autoincrement=True)
+    lessonId: Mapped[int] = mapped_column(Integer, ForeignKey('lesson.lessonId'))
+    studentId: Mapped[int] = mapped_column(Integer, ForeignKey('student.studentId'))
+    gradebook5: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    gradebook4: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    gradebook3: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    gradebook2: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+    gradebookVisits: Mapped[int] = mapped_column(Integer, use_existing_column=False)
+
+class Admin(Base):
+    __tablename__ = 'admin'
+
+    adminId: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    adminTelegram_id: Mapped[int] = mapped_column(Integer)
+    adminPassword: Mapped[str] = mapped_column(String(64), use_existing_column=False)
--- db/orm_query.py
+++ db/orm_query.py
@@ -1,37 +1,87 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from db.models import Teacher, Lesson
+from models import Teacher, Lesson, Student, Gradebook, Admin
 from sqlalchemy import select
 from parser import Lessons
 
 
 async def orm_add_teacher(session: AsyncSession, data: dict):
-# Создание преподавателя
-        teach = Teacher(
-            teacherTelegram_id=data['id'],
-            teacherSurname=data['name_and_post'][0],
-            teacherName=data['name_and_post'][1],
-            teacherPatronymic=data['name_and_post'][2],
-            teacherPosition=data['name_and_post'][3],
-            teacherPassword=data['password']
-        )
+    # Создание преподавателя
+    teach = Teacher(
+        teacherTelegram_id=data['id'],
+        teacherSurname=data['name_and_post'][0],
+        teacherName=data['name_and_post'][1],
+        teacherPatronymic=data['name_and_post'][2],
+        teacherPosition=data['name_and_post'][3],
+        teacherPassword=data['password']
+    )
 
-        # Добавление преподавателя в таблицу
-        session.add(teach)
-        await session.commit()
+    # Добавление преподавателя в таблицу
+    session.add(teach)
+    await session.commit()
+
 
 async def orm_get_teacher(session: AsyncSession, tg_id: int):
     query = select(Teacher).where(Teacher.teacherTelegram_id == tg_id)
     result = await session.execute(query)
     return result.scalar()
 
-async def orm_add_lesson(session: AsyncSession, data: dict, lesson:Lessons):
-# Создание преподавателя
-        les = Lesson(
-            teacherTelegram_id=data['id'],
-            LessonName=lesson[2],
-            LessontType=lesson[1],
-            Group=lesson[0]
-        )
 
-        session.add(les)
-        await session.commit()
+async def orm_add_lesson(session: AsyncSession, data: dict, lesson: Lessons):
+    # Создание занятия
+    les = Lesson(
+        teacherTelegram_id=data['id'],
+        LessonName=lesson[2],
+        LessontType=lesson[1],
+        Group=lesson[0]
+    )
+
+    session.add(les)
+    await session.commit()
+
+# async def orm_add_student(session: AsyncSession, data: dict):
+#     # Создание студента
+#     stud = Student(
+#         studentId=data['id'],
+#         studentGroup=data['group'],
+#         studentSurname=data['name'][0],
+#         studentName=data['name'][1],
+#         studentPatronymic=data['name'][2],
+#         studentTelegram_id=data['tg_id']
+#     )
+#
+#     # Добавление преподавателя в таблицу
+#     session.add(stud)
+#     await session.commit()
+#
+#
+# async def orm_get_student(session: AsyncSession, id: int):
+#     query = select(Student).where(Student.studentId == id)
+#     result = await session.execute(query)
+#     return result.scalar()
+#
+# async def orm_add_gradebook(session: AsyncSession, data: dict):
+#     # Создание дневника
+#     grade_b = Gradebook(
+#         lessonId=data['les_id'],
+#         studentId=data['st_id'],
+#         gradebook5=0,
+#         gradebook4=0,
+#         gradebook3=0,
+#         gradebook2=0,
+#         gradebookVisits=0
+#     )
+#
+#     # Добавление преподавателя в таблицу
+#     session.add(grade_b)
+#     await session.commit()
+#
+# async def orm_add_admin(session: AsyncSession, data: dict):
+#     # Создание дневника
+#     adm = Admin(
+#         adminTelegram_id=data['tg_id'],
+#         adminPassword=data['password']
+#     )
+#
+#     # Добавление преподавателя в таблицу
+#     session.add(adm)
+#     await session.commit()
